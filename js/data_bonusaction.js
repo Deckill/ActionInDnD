200a900ec471f9b09ceb6557a1aef2b23a2bd4e8
Deckill <35498686+Deckill@users.noreply.github.com>
2020-11-27 13:10:46 +0900

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -1,34 +1,33 @@
 data_bonusaction = [
     {
-        title: "Offhand Attack",
+        title: "오프핸드 공격/Offhand Attack",
         icon: "crossed-swords",
-        subtitle: "Use with the Attack action",
-        description: "Attack with your off hand",
+        subtitle: "공격 행동과 같이 사용",
+        description: "반대편 손으로 공격합니다.",
         reference: "PHB, pgs. 192,194-195.",
         bullets: [
-            "Only usable if you take the <i>Attack</i> action and attack with a light melee weapon that you're holding in one hand.",
-            "Perform a single attack with a different light melee weapon that you're holding in the other hand.",
-            "You don't add your ability modifier to the damage of the bonus attack, unless that modifier is negative.",
-            "If either weapon has the thrown property, you can throw the weapon, instead of making a melee attack with it."
+            "당신이 한 손에 드는 경량형 근접 무기를 사용하여 공격 <i>행동</i>을 취한 경우, 추가 행동을 사용하여 반대편 손에 쥐고 있는 다른 경량형 근접 무기로도 공격을 가할 수 있습니다.",
+            "두 번째 공격의 경우, 피해 굴림에는 당신의 능력 수정치를 더할 수 없습니다. 만약 수정치가 페널티를 가할 경우, 그 페널티는 적용됩니다.",
+            "만약 양손에 든 무기 중 어느 한쪽이 투척 속성을 지닌 경우, 근접 공격을 가하는 대신 그것을 던질 수도 있습니다."
         ]
     },
     {
-        title: "Cast a spell",
+        title: "주문 시전/Cast a spell",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action",
-        description: "Cast a spell with a casting time of 1 bonus action",
+        subtitle: "1 추가 행동의 시전 시간",
+        description: "1 추가 행동의 시전 시간을 가진 주문을 시전합니다.",
         reference: "PHB, pg. 192.",
         bullets: [
-            "You can't cast a spell with you action and a different spell with your bonus action in the same turn, except if the action is used to cast a cantrip.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "추가 행동으로 주문을 시전할 경우, 시전 시간이 1 행동인 소마법들을 제외하면 해당 턴에 또 다른 주문을 시전할 수 없습니다.",
+            "자세한 내용은 <i>주문 시전</i>행동 참조."
         ]
     },
     {
-        title: "Use class feature",
+        title: "클래스 요소를 사용/Use class feature",
         icon: "embrassed-energy",
-        subtitle: "Some features use bonus actions",
-        description: "Use a racial or class feature that uses a bonus action",
-        reference: "See class page for more information.",
+        subtitle: "몇몇 클래스 요소는 추가 행동을 요구",
+        description: "추가 행동을 요구하는 종족 또는 클래스 요소를 사용합니다.",
+        reference: "제3장 클래스 요소를 참조하십시오.",
         bullets: [
 
         ]
