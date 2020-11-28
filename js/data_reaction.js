6b88df3738fe3f26dc9ae17e47466d90ae40b6fc
Deckill <35498686+Deckill@users.noreply.github.com>
2020-11-28 19:58:15 +0900

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,15 +1,16 @@
 data_reaction = [
     {
-        title: "Opportunity attack",
+        title: "기회공격/Opportunity attack",
         icon: "crossed-swords",
         subtitle: "Enemy leaves your reach",
-        description: "You can rarely move heedlessly past your foes without putting yourself in danger",
+        description: "전투 상황에서는 모두가 적이 방어에 헛점을 보이길 감시하고 있습니다.",
         reference: "PHB, pg. 195.",
         bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
-            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
+            "당신은 자신이 볼 수 있는 적대적 크리쳐가 당신의 공격 범위를 벗어나려는 순간 기회공격을 가할 수 있습니다.",
+            "당신은 기회공격을 유발한 크리쳐에게 한 번의 근접 공격을 가할 수 있습니다.",
+            "기회공격은 해당 크리쳐가 당신의 간격을 벗어나기 직전에 가해집니다.",
+            "퇴각 행동을 사용할 경우 기회공격을 유발하는 것을 피할 수 있습니다.",
+            "또한 순간이동을 하거나 다른 누군가, 혹은 무엇인가에 의해 강제로 이동된 경우 역시 기회공격을 유발하지 않습니다."
         ]
     },
     {
